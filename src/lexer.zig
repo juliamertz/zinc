d07fcea7ee31428f80aaca221a1caad620dd0587
Julia Mertz <info@juliamertz.dev>
2025-01-15 20:46:28 +0100

    wip

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const Token = union(enum) {
+pub const Token = union(enum) {
     ident: []const u8,
     integer: []const u8,
 
@@ -13,16 +13,50 @@ const Token = union(enum) {
     eof,
 };
 
-const Lexer = struct {
+pub const Lexer = struct {
     const Self = @This();
 
     pos: usize = 0,
     content: []const u8,
 
-    pub fn current(self: *Self) u8 {
-        return self.content[self.pos];
+    fn current(self: *Self) ?u8 {
+        if (self.pos < 0 or self.pos >= self.content.len) {
+            return null;
+        }
+        return self.content[self.pos - 1];
+    }
+
+    fn advance(self: *Self) ?u8 {
+        if (self.pos < self.content.len) {
+            self.pos += 1;
+            return self.current();
+        }
+        return null;
+    }
+
+    fn next_token(self: *Self) ?Token {
+        if (self.advance()) |ch| {
+            return switch (ch) {
+                '=' => .assign,
+                '+' => .plus,
+                else => .illegal,
+            };
+        }
+
+        return null;
     }
 };
 
-// pub fn tokenize() []Token {
-// }
+pub fn tokenize(content: []const u8) ![]Token {
+    var lexer = Lexer{ .content = content, .pos = 0 };
+
+    var tokens = std.ArrayList(Token).init(std.heap.page_allocator);
+    defer tokens.deinit();
+
+    while (lexer.next_token()) |tok| {
+        try tokens.append(tok);
+        // std.debug.print("current: {}\n", .{tok});
+    }
+
+    return tokens.items;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,22 @@
 const std = @import("std");
+const lexer = @import("./lexer.zig");
 
 pub fn main() !void {
-    const file = try std.fs.cwd().openFile("spec/1.mlang", .{});
-    defer file.close();
-    const contents = try file.readAllAlloc(std.heap.GeneralPurposeAllocator.init(), .{});
-    std.debug.print("hello world, {}\n", .{contents});
+    const alloc = std.heap.page_allocator;
+
+    const data = try std.fs.cwd().readFileAlloc(alloc, "spec/1.mlang", std.math.maxInt(usize));
+    defer alloc.free(data);
+
+    std.debug.print("{s}", .{data});
+
+    _ = try lexer.tokenize(data);
+    // std.debug.print("{}\n", .{tokens});
+
+    // var lex = lexer.Lexer{ .content = data, .pos = 0 };
+    // const curr = lex.current();
+    // std.debug.print("current char: {c}\n", .{curr});
+    // const next = lex.advance();
+    // if (next) |ch| {
+    //     std.debug.print("current char: {c}\n", .{ch});
+    // }
 }
